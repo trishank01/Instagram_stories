ac71b19f286aee8338df6ab405de13ced3527977
tk <trishank01@gmail.com>
2025-05-25 13:50:03 +0530

    now user can click on image right/left side to navigate

--- src/components/StoryItem.jsx
+++ src/components/StoryItem.jsx
@@ -1,22 +1,54 @@
-
-import { useParams } from "react-router-dom";
-
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const StoryItem = () => {
   const { id } = useParams();
 
+  const navigate = useNavigate();
+
+  const [storyId, setStoryId] = useState(+id);
+
+  const backToHome = () => {
+    navigate("/");
+  };
+
+  const leftClick = () => {
+    setStoryId(storyId + 1);
+  };
+
+  const rightClick = () => {
+    setStoryId(storyId - 1);
+  };
 
   return (
     <div>
-      <div className="flex flex-col text-center gap-4 w-[400px] h-screen border-2 border-black px-2 rounded-md">
+      <div className="flex flex-col text-center gap-4 w-[400px] h-screen px-2 rounded-md">
         <h2 className="text-2xl text-red-300">Instagram Stories</h2>
-   
-          <div className="flex flex-col items-center justify-between">
-            <div className="rightSide"></div>
-            <img className="w-fit rounded-md shadow-2xl" src={`https://picsum.photos/id/${id}/300/500`} alt="" />
-               <div className="leftSide"></div>
-          </div>
-   
+        <button
+          onClick={() => backToHome()}
+          className="text-blue-500 cursor-pointer"
+        >
+          Home
+        </button>
+
+        <div className="relative flex flex-col items-center justify-between w-full h-[500px] ">
+          <img
+            className="w-full rounded-md shadow-2xl"
+            src={`https://picsum.photos/id/${storyId}/300/500`}
+            alt=""
+          />
+          {/* Left clickable half */}
+          <div
+            className="absolute top-0 left-0 w-1/2 h-full z-10 cursor-pointer"
+            onClick={() => leftClick()}
+          ></div>
+
+          {/* Right clickable half */}
+          <div
+            className="absolute top-0 right-0 w-1/2 h-full z-10 cursor-pointer"
+            onClick={() => rightClick()}
+          ></div>
+        </div>
       </div>
     </div>
   );
